c51839c9277d808dee399a177668267ed659812f
ompatel-2112 <ompatel43456@gmail.com>
2026-01-02 13:02:18 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,6 @@
+name = input("Enter your name:")
+x = int(input("Enter your age :"))
+
+print(f"type of name : {type(name)} ")
+print(f"type of age : {type(x)} ")
+print(f"my name is {name} and my age is {x}")
