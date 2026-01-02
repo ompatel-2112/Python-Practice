1f8a959e3c4baef09572445cc11eac676c96053e
ompatel-2112 <ompatel43456@gmail.com>
2026-01-02 14:30:12 +0530

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,6 +1,38 @@
+
+#python syntax
+print("Never Give Up!")
+
+#Case Sensitive
+
+age= 20  
+        # upper case and lower case is Different in python
+Age= 25
+
+#Variable - used for store data
+
+x = 10
+
+name = "om"
+
+# Dynamic typing
+
+""" which auto matically detacts the data type of the variable 
+    we don't need to define type like int,char,string
+"""
+p = 10 #int
+q = "queen" #string
+
+# input from user
+
 name = input("Enter your name:")
-x = int(input("Enter your age :"))
+
+#type casting for taking integer value from user
+a = int(input("Enter your age :"))
 
 print(f"type of name : {type(name)} ")
 print(f"type of age : {type(x)} ")
+
 print(f"my name is {name} and my age is {x}")
+
+
+
