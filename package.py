27f1e2e666acb7773c69f0b370059012fda81cbc
ompatel-2112 <ompatel43456@gmail.com>
2026-01-21 12:50:54 +0530

    package.py

---
+++ package.py
@@ -0,0 +1,30 @@
+# package : collection of modules/py files + __init__.py
+
+# Module = a single .py file
+# Package = a folder containing multiple module
+
+# Why packages  :
+"""
+Packages help to:
+
+- Organize large programs
+
+- Avoid name conflicts
+
+- Improve code reusability
+
+- Make code easier to maintain
+
+- Group related functionality
+
+"""
+# structure of package :
+
+"""
+my_package/                  # package
+|                           
+|--> __init__.py              # tells py this folder is package
+|--> math_operations.py       # module
+|--> string_operations.py     # module
+
+"""
